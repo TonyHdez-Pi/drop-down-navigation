aac46fd319992792b66c2bfeb38ca6cf36bd0375
Antonio Hernandez <tonyhdezpi@gmail.com>
2022-06-09 14:06:54 -0500

    Finished the desktop page, added javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+let dropdownItemFeatures = document.getElementById("menuItem__features");
+let dropdownItemCompany =  document.getElementById("menuItem__company");
+console.log(dropdownItemCompany);
+console.log(dropdownItemFeatures);
+
+dropdownItemCompany.addEventListener("click", );

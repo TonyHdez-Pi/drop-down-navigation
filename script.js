8d50c15ae1c01fa8da22453e3cdbce157be71cbc
Antonio Hernandez <tonyhdezpi@gmail.com>
2022-06-15 16:27:11 -0500

    trying to make the buttons to work

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-const featuresButton = document.querySelector(".featuresClick");
-const featuresMenu = document.querySelector(".featuresMenu");
-const companyButton = document.querySelector(".companyClick");
-const companyMenu = document.querySelector(".companyMenu");
+// Getting the id for the button that is gonna be clicked
+const menuItem = document.getElementsByClassName("menuItem");
+// Getting the elements that gonna be displayed
+const dropdownItem = document.getElementsByClassName("subMenu");
 
-companyButton.addEventListener("click", ()  =>{
-    companyMenu.classList.remove("hideMenu");
-});
-// companyButton.addEventListener("click", ()  =>{
-//     companyMenu.classList.add("hideMenu");
-// });
-featuresButton.addEventListener("click", ()  =>{
-    featuresMenu.classList.remove("hideMenu");
-});
-// featuresButton.addEventListener("click", ()  =>{
-//     companyMenu.classList.add("hideMenu");
-// });
-
-console.log(2+2) 

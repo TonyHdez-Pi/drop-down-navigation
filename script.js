1c83e34e492f42dbc0afb15815db24051702f61d
Antonio Hernandez <tonyhdezpi@gmail.com>
2022-06-17 11:53:45 -0500

    finally done with javascript

--- script.js
+++ script.js
@@ -2,4 +2,20 @@
 const menuItem = document.getElementsByClassName("menuItem");
 // Getting the elements that gonna be displayed
 const dropdownItem = document.getElementsByClassName("subMenu");
+var isOpen = false;
 
+menuItem[0].addEventListener("click", showHideMenu(0));
+menuItem[1].addEventListener("click", showHideMenu(1));
+
+
+function showHideMenu(num){
+  menuItem[num].addEventListener("click", () => {
+    if (isOpen == false) {
+      dropdownItem[num].classList.remove("hide");
+      isOpen = true;
+    } else if (isOpen == true) {
+      dropdownItem[num].classList.add("hide");
+      isOpen = false;
+    }
+  });
+}
